4021772ffb93ec4d42d9694e9d856f136a99f36e
cufa <facundonic.gonzalez@gmail.com>
2025-11-25 11:25:49 -0300

    Actualizacion index

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // ===========================
 // CONFIGURACIÓN DE PROYECTOS
 // ===========================
@@ -334,28 +333,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
-=======
- // Contenedor de galería
-    const gallery = document.getElementById('portfolio');
-
-    // Cantidad de imágenes que tenés en la carpeta
-    const totalImages = 96;
-
-    // Generar array con nombres secuenciales .webp
-    let images = [];
-    for (let i = 1; i <= totalImages; i++) {
-      images.push(`photos/photo${i}.webp`);
-    }
-
-    // Mezclar aleatoriamente
-    images = images.sort(() => Math.random() - 0.5);
-
-    // Renderizar en el DOM
-    images.forEach(src => {
-      const img = document.createElement('img');
-      img.src = src;
-      img.alt = "Fotografía de CUFA";
-      img.loading = 'lazy';
-      gallery.appendChild(img);
-    });
->>>>>>> 756f2d96978fbd92415f22d6ffa35d324259bacc
